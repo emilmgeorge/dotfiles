ded79b199009c7bb81dde4c7c43f968ae85eca42
Emil M George <emilmgeorge@gmail.com>
2022-10-18 19:52:39 +0530

    (nvim) Fix configs after plugin update

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -22,7 +22,7 @@ function M.configure()
 
 	-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+	capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 	require 'lspconfig'.clangd.setup {
 		on_attach = on_attach,
