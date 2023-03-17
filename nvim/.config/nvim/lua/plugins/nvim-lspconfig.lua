802661fa1ba871f0b5019983c2e3ce7360d10bee
Emil M George <emilmgeorge@gmail.com>
2023-03-17 14:38:23 +0530

    (nvim) Fix Lua LSP after name change
    Lua LSP sumneko_lua has been renamed to lua_ls
    https://github.com/neovim/nvim-lspconfig/pull/2439

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -29,7 +29,7 @@ function M.configure()
 		capabilities = capabilities,
 	}
 
-	require 'lspconfig'.sumneko_lua.setup {
+	require 'lspconfig'.lua_ls.setup {
 		on_attach = on_attach,
 		settings = {
 			Lua = {
