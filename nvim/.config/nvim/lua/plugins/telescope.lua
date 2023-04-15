044cecdf63ffd75eac9d47174d74e940c372f267
Emil M George <emilmgeorge@gmail.com>
2023-04-15 23:06:57 +0530

    (nvim) Bump Telescope up to 0.1.1 and add a few keybindings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,14 +8,23 @@ function M.configure()
 		["<leader>g"] = { name = "+go" },
 		["<leader>gg"] = { "<cmd>Telescope lsp_definitions fname_width=40<cr>", "Go to definitions" },
 		["<leader>gr"] = { "<cmd>Telescope lsp_references fname_width=40<cr>", "Go to references" },
-		["<leader>gs"] = { "<cmd>Telescope lsp_document_symbols fname_width=40<cr>", "Go to symbol" },
+		["<leader>gs"] = { "<cmd>Telescope lsp_document_symbols fname_width=40<cr>", "Go to document symbols" },
+		["<leader>gws"] = { "<cmd>Telescope lsp_dynamic_workspace_symbols fname_width=40<cr>", "Go to workspace symbols" },
+		["<leader>gic"] = { "<cmd>Telescope lsp_incoming_calls fname_width=40<cr>", "Go to Incoming Calls" },
+		["<leader>goc"] = { "<cmd>Telescope lsp_incoming_calls fname_width=40<cr>", "Go to Outgoing Calls" },
+
+		["<leader>gj"] = { "<cmd>Telescope jumplist fname_width=40<cr>", "Go to jumplist history" },
+		["<leader>gb"] = { "<cmd>Telescope buffers fname_width=40<cr>", "Go to buffer" },
+		["<leader>gf"] = { "<cmd>Telescope find_files fname_width=40<cr>", "Go to file" },
+		["<leader>ggf"] = { "<cmd>Telescope git_files fname_width=40<cr>", "Go to file" },
+		["<leader>gy"] = { "<cmd>Telescope yank_history fname_width=40<cr>", "Yank History" },
 	})
 end
 
 function M.setup()
 	require 'packer'.use {
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.0',
+		tag = '0.1.1',
 		requires = 'nvim-lua/plenary.nvim',
 		config = M.configure,
 	}
