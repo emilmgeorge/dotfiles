2495e2f74b2b2d3b3ed0241ef6718503633df97e
Emil M George <emilmgeorge@gmail.com>
2023-04-29 14:16:43 +0530

    (nvim) Handle error: saved location of cursor is outside file

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -26,7 +26,12 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function()
 		local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
 		if {row, col} ~= {0, 0} then
-			vim.api.nvim_win_set_cursor(0, {row, col})
+			local status, err = pcall(function()
+				vim.api.nvim_win_set_cursor(0, {row, col})
+			end)
+			if status == false then
+				vim.notify(vim.inspect(err))
+			end
 		end
 	end,
 	group = vim.api.nvim_create_augroup("jump_last_position", { clear = true })
