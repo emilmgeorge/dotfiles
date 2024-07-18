de482f90596d0eeba4c3ff3d8d3b18ea17e4a61b
Emil M George <emilmgeorge@gmail.com>
2024-07-19 01:59:41 +0530

    (nvim-lazy) Disable autoformat and autosave

--- nvim-lazy/.config/nvim-lazy/lua/config/options.lua
+++ nvim-lazy/.config/nvim-lazy/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable LazyVim auto-format on save
+vim.g.autoformat = false
+
+-- Disable auto-write on buffer switch
+vim.opt.autowrite = false
