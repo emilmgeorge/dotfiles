dedc3861cb3b318d2683c45b46cb332888babcd2
Emil M George <emilmgeorge@gmail.com>
2024-07-18 16:24:19 +0530

    (nvim-lazy) Use lazy.nvim SHA from lock-file during bootstrap

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -32,7 +32,7 @@ function M.setup(spec)
 		end
 		-- Clone lazy.nvim
 		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-		local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+		local out = vim.fn.system({ "git", "clone", "--branch=stable", lazyrepo, lazypath })
 		if vim.v.shell_error ~= 0 then
 			vim.api.nvim_echo({
 				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
