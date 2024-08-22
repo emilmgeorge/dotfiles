fda6ac1a0e6065cada546fd4a2187b0d1dadb980
Emil M George <emilmgeorge@gmail.com>
2024-08-22 20:41:44 +0530

    (nvim) Use upstream cscope_maps instead of patched version

--- nvim/.config/nvim/lua/plugins/cscope_maps.lua
+++ nvim/.config/nvim/lua/plugins/cscope_maps.lua
@@ -1,12 +1,19 @@
 local M = {}
 
 function M.configure()
-	require 'cscope_maps'
+	require('cscope_maps').setup({
+		skip_input_prompt = false,
+		prefix = '<C-\\>',
+		cscope = {
+			picker = 'telescope',
+			db_build_cmd_args = { '-bqRv' },
+		}
+	})
 end
 
 function M.setup()
 	return {
-		'emilmgeorge/cscope_maps.nvim',
+		'dhananjaylatkar/cscope_maps.nvim',
 		config = M.configure,
 	}
 end
