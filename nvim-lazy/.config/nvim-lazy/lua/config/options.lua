661416ae6c80fe50627c4452c2528a51e03b0d42
Emil M George <emilmgeorge@gmail.com>
2024-09-09 23:34:17 +0530

    (nvim-lazy) Personalize lazyvim (1)

--- nvim-lazy/.config/nvim-lazy/lua/config/options.lua
+++ nvim-lazy/.config/nvim-lazy/lua/config/options.lua
@@ -21,3 +21,53 @@ vim.opt.autowrite = false
 -- Note 2: <Alt-*> bindings may still be triggered in some cases, especially
 --         during remote sessions.
 vim.o.ttimeoutlen = 0
+
+-- Allow moving to one char position beyond last char in a line
+vim.opt.virtualedit = "onemore"
+
+-- Whitespace display
+vim.opt.listchars = "eol:⬎,tab:│─,space:˽,extends:→,precedes:←,nbsp:⍽"
+vim.opt.list = false
+
+-- Formatting options
+vim.opt.wrap = true
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "80"
+vim.opt.formatoptions = "crqn2l1j"
+vim.opt.cinoptions = ":0,l1,g0,t0,(0"
+
+-- Set format for fold text
+vim.cmd([[
+function! GetSpaces(foldLevel)
+    if &expandtab == 1
+        " Indenting with spaces
+        let str = repeat(" ", a:foldLevel / (&shiftwidth + 1) - 1)
+        return str
+    elseif &expandtab == 0
+        " Indenting with tabs
+        return repeat(" ", indent(v:foldstart) - (indent(v:foldstart) / &shiftwidth))
+    endif
+endfunction
+
+function! MyFoldText()
+    let startLineText = getline(v:foldstart)
+    let secondLineText = trim(getline(v:foldstart + 1))
+    let endLineText = trim(getline(v:foldend))
+	let linecount = v:foldend - v:foldstart - 1
+    let indentation = GetSpaces(foldlevel("."))
+    let end = repeat("-", 200)
+
+	if secondLineText != "{"
+		let secondLineText = ""
+	else
+		let secondLineText = " " . secondLineText
+	endif
+
+    let str = indentation . startLineText . secondLineText . " ... " . endLineText . " [" . linecount . " lines] " . end
+
+    return str
+endfunction
+
+" Custom display for text when folding
+set foldtext=MyFoldText()
+]])
