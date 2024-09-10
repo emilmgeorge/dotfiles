f38459e3485992bc083c01f3863c05a850e9c0db
Emil M George <emilmgeorge@gmail.com>
2024-09-11 01:15:45 +0530

    (nvim-lazy) Use ra-multiplex for rust-analyzer

--- nvim-lazy/.config/nvim-lazy/lua/plugins/overrides/lsp.lua
+++ nvim-lazy/.config/nvim-lazy/lua/plugins/overrides/lsp.lua
@@ -1,9 +1,25 @@
+-- Rust tools installed via rustup.
+-- ra-multiplex installed via distro/external packager managers.
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Start ra-multiplex server",
+  once = true,
+  pattern = "rust",
+  command = "silent !systemctl --user start ra-multiplex.service",
+})
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = {
         enabled = false,
+      },
+      servers = {
+        rust_analyzer = {
+          cmd = {
+            "/usr/bin/ra-multiplex",
+          },
+        },
       },
     },
   },
