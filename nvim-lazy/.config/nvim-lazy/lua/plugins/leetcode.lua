834259bedb2a95741a04de0335c0e366577bd340
Emil M George <emilmgeorge@gmail.com>
2025-04-02 17:44:14 +0530

    (nvim-lazy) Add leetcode.nvim

---
+++ nvim-lazy/.config/nvim-lazy/lua/plugins/leetcode.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    cmd = "Leet",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      injector = {
+        ["python3"] = {
+          before = true,
+        },
+        ["cpp"] = {
+          before = { "#include <bits/stdc++.h>", "using namespace std;" },
+          after = "int main() {}",
+        },
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if opts and opts.dashboard and opts.dashboard.preset and opts.dashboard.preset.keys then
+        local keys = opts.dashboard.preset.keys
+        local leet_key = { icon = "{}", key = "L", desc = "Leetcode", action = ":Leet" }
+        if keys[#keys].desc == "Quit" then
+          table.insert(keys, #keys, leet_key)
+        else
+          table.insert(keys, leet_key)
+        end
+      end
+    end,
+  },
+}
