e643c189616710011326b7117d0d269efceb3965
Emil M George <emilmgeorge@gmail.com>
2025-04-29 14:57:17 +0530

    (nvim-lazy) Add and configure extras.editor.mini-files

---
+++ nvim-lazy/.config/nvim-lazy/lua/plugins/overrides/extras/editor/mini-files.lua
@@ -0,0 +1,14 @@
+return {
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
+  {
+    "echasnovski/mini.files",
+    opts = {
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_nofocus = 30,
+        width_preview = 30,
+      },
+    },
+  },
+}
