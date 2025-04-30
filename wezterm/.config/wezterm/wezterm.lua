f69b0621a2dab9bd4f3a33aa69a8ab05f79478ee
Emil M George <emilmgeorge@gmail.com>
2025-05-01 02:26:21 +0530

    (wezterm) Revamp key bindings (modal)

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,25 +14,81 @@ config.window_decorations = 'NONE'
 -- Disable ligatures
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
--- Key bindings
+-- Key bindings ---------------------------------------------------------------|
+-- Disable default bindings
+config.disable_default_key_bindings = true
+
+-- Clipboard
+table.insert(config.keys, { key = 'c', mods = 'CTRL|SHIFT', action = wezterm.action.CopyTo('ClipboardAndPrimarySelection') })
+table.insert(config.keys, { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.PasteFrom('Clipboard') })
+table.insert(config.keys, { key = 'Insert', mods = 'SHIFT', action = wezterm.action.PasteFrom('Clipboard') })
+
+-- Set leader key
+local leader_mod = 'CTRL'
+local leader_root = 'q'
+config.leader = { key = leader_root, mods = leader_mod, timeout_milliseconds = 5000 }
+table.insert(config.keys, { key = leader_root, mods = 'LEADER|' .. leader_mod, action = wezterm.action.SendKey { key = leader_root, mods = leader_mod }})
+
+-- View
+table.insert(config.keys, { key = 'z', mods = 'LEADER', action = wezterm.action.ToggleFullScreen })
+table.insert(config.keys, { key = '+', mods = 'LEADER|SHIFT', action = wezterm.action.IncreaseFontSize })
+table.insert(config.keys, { key = '-', mods = 'LEADER', action = wezterm.action.DecreaseFontSize })
+table.insert(config.keys, { key = '=', mods = 'LEADER', action = wezterm.action.ResetFontSize })
+
+-- Modes
+table.insert(config.keys, { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode})
+table.insert(config.keys, { key = '/', mods = 'LEADER', action = wezterm.action.Search('CurrentSelectionOrEmptyString')})
+
+-- Misc
+table.insert(config.keys, { key = 'u', mods = 'LEADER|CTRL|SHIFT', action = wezterm.action.CharSelect({ copy_on_select = true, copy_to = 'ClipboardAndPrimarySelection' }) })
+table.insert(config.keys, { key = 'Space', mods = 'LEADER', action = wezterm.action.QuickSelect })
+
 -- Tab control
 for i = 1,9 do
-  table.insert(config.keys, { key = tostring(i), mods = 'ALT', action = wezterm.action.ActivateTab(i - 1) })
+  table.insert(config.keys, { key = tostring(i), mods = 'LEADER', action = wezterm.action.ActivateTab(i - 1) })
 end
-table.insert(config.keys, { key = '[', mods = 'ALT', action = wezterm.action.ActivateTabRelative(-1) })
-table.insert(config.keys, { key = ']', mods = 'ALT', action = wezterm.action.ActivateTabRelative(1) })
-table.insert(config.keys, { key = '[', mods = 'ALT|SHIFT', action = wezterm.action.MoveTabRelative(-1) })
-table.insert(config.keys, { key = ']', mods = 'ALT|SHIFT', action = wezterm.action.MoveTabRelative(1) })
-table.insert(config.keys, { key = 'w', mods = 'ALT', action = wezterm.action.CloseCurrentTab { confirm = true }})
+table.insert(config.keys, { key = 'p', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) })
+table.insert(config.keys, { key = 'n', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) })
+table.insert(config.keys, { key = 'H', mods = 'LEADER|SHIFT', action = wezterm.action.MoveTabRelative(-1) })
+table.insert(config.keys, { key = 'L', mods = 'LEADER|SHIFT', action = wezterm.action.MoveTabRelative(1) })
+table.insert(config.keys, { key = '&', mods = 'LEADER', action = wezterm.action.CloseCurrentTab { confirm = true }})
+table.insert(config.keys, { key = "c", mods = "LEADER", action = wezterm.action.SpawnCommandInNewTab({
+    args = { os.getenv("SHELL"), "-l" },
+    set_environment_variables = { SKIP_MUX_INIT = "1" },
+}) })
 
--- Add launch menu
+-- Pane control
+table.insert(config.keys, { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") })
+table.insert(config.keys, { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") })
+table.insert(config.keys, { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") })
+table.insert(config.keys, { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") })
+table.insert(config.keys, { key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) })
+table.insert(config.keys, { key = '%', mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({
+    args = { os.getenv("SHELL"), "-l" },
+    set_environment_variables = { SKIP_MUX_INIT = "1" },
+}) })
+table.insert(config.keys, { key = "\"", mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical({
+    args = { os.getenv("SHELL"), "-l" },
+    set_environment_variables = { SKIP_MUX_INIT = "1" },
+}) })
+
+-- Launch menu
 local launch_menu = {}
 if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
   table.insert(launch_menu, { label = 'zsh without tmux', args = { 'zsh', '-l' }, set_environment_variables = { SKIP_MUX_INIT = '1' } })
   table.insert(launch_menu, { label = 'bash without tmux', args = { 'bash', '-l' }, set_environment_variables = { SKIP_MUX_INIT = '1' } })
 end
 config.launch_menu = launch_menu
-table.insert(config.keys, { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncherArgs { flags = 'LAUNCH_MENU_ITEMS' } })
-table.insert(config.keys, { key = 'L', mods = 'ALT', action = wezterm.action.ShowLauncher })
+table.insert(config.keys, { key = 'l', mods = 'LEADER|CTRL', action = wezterm.action.ShowLauncher })
+
+-- On the fly configuration
+local features = require('features')
+table.insert(config.keys, { key = "c", mods = "LEADER|CTRL", action = wezterm.action_callback(features.theme_switcher) })
+table.insert(config.keys, { key = "f", mods = "LEADER|CTRL", action = wezterm.action_callback(features.font_switcher) })
+
+-- Wezterm-related
+table.insert(config.keys, { key = 'p', mods = 'LEADER|CTRL', action = wezterm.action.ActivateCommandPalette})
+table.insert(config.keys, { key = 's', mods = 'LEADER', action = wezterm.action.ReloadConfiguration})
+table.insert(config.keys, { key = 'd', mods = 'LEADER|ALT', action = wezterm.action.ShowDebugOverlay})
 
 return config
