8d37e4203e5c49d111412e2973a923d5654679e9
Emil M George <emilmgeorge@gmail.com>
2025-05-25 11:10:53 +0530

    (tmux) Do TPM bootstrap in the background

---
+++ tmux/.config/tmux/bootstrap-tpm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+tmux set -g status-right-length 100
+
+tmux set -gF status-right "#[bg=red] TPM Bootstrap: downloading TPM... "
+mkdir -p "${TMUX_PLUGIN_MANAGER_PATH}"
+git clone https://github.com/tmux-plugins/tpm "${TMUX_PLUGIN_MANAGER_PATH}/tpm"
+
+tmux set -gF status-right "#[bg=red] TPM Bootstrap: installing plugins... "
+"${TMUX_PLUGIN_MANAGER_PATH}/tpm/bin/install_plugins"
+
+tmux set -gF status-right "#[bg=red] TPM Bootstrap: re-sourcing tmux config... "
+tmux source-file "${TMUX_CONFIG_DIR}/tmux.conf"
