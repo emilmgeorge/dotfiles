767745b843501d3802a16360f79808dee9e42ef1
Emil M George <emilmgeorge@gmail.com>
2025-05-25 11:10:53 +0530

    (tmux) Patch tpm during bootstrap

--- tmux/.config/tmux/bootstrap-tpm.sh
+++ tmux/.config/tmux/bootstrap-tpm.sh
@@ -6,6 +6,9 @@ tmux set -gF status-right "#[bg=red] TPM Bootstrap: downloading TPM... "
 mkdir -p "${TMUX_PLUGIN_MANAGER_PATH}"
 git clone https://github.com/tmux-plugins/tpm "${TMUX_PLUGIN_MANAGER_PATH}/tpm"
 
+tmux set -gF status-right "#[bg=red] TPM Bootstrap: patching TPM... "
+git -C "${TMUX_PLUGIN_MANAGER_PATH}/tpm" am ${TMUX_CONFIG_DIR}/plugin-files/tpm/patches/*.patch
+
 tmux set -gF status-right "#[bg=red] TPM Bootstrap: installing plugins... "
 "${TMUX_PLUGIN_MANAGER_PATH}/tpm/bin/install_plugins"
 
