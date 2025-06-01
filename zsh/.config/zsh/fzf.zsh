6b5dd7d2a9c61b234610b6cd2fc2cb97970f0891
Emil M George <emilmgeorge@gmail.com>
2025-06-01 16:16:11 +0530

    (zsh) fzf-history-widget: add header/info/preview

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -14,11 +14,33 @@ functions -c fzf-history-widget fzf-history-widget-orig
 fzf-history-widget() {
 	local SEP="\x1F"
 
+	local HEADER_TEXT=$(printf "%s\n%s\n%s\n%s\n%s\n%s" \
+		$'\e[2;33mCtrl-D\e[0m => Toggle deletion' \
+		$'\e[2;33mCtrl-L\e[0m => Clear deletions ($FZF_SELECT_COUNT)' \
+		$'\e[2;33mCtrl-F\e[0m => Entry Info' \
+		$'\e[2;33mCtrl-X\e[0m => Delete & Close' \
+		$'\e[2;33mEnter\e[0m  => Delete & Accept' \
+		$'\e[2;33mEscape\e[0m => Close' \
+	)
+
 	local DELETE_TRANSFORM="[ \"\$FZF_SELECT_COUNT\" -gt 0 ] && printf \"+print(${SEP}d${SEP}%s)\" \"{r+1}\";"
 	FZF_CTRL_R_OPTS="
+	--info-command 'echo -e \"\$FZF_POS/\$FZF_INFO\"'
+	--preview 'column -d -S 5 -c \$(tput cols) <<< \"$HEADER_TEXT\"'
+	--preview-window 'up,2,nowrap'
+	--bind 'resize:refresh-preview'
 	--bind 'start:change-multi'
 	--bind 'ctrl-d:toggle'
 	--bind 'ctrl-l:deselect-all'
+	--bind 'ctrl-f:execute(
+		clear;
+		HISTSIZE=$HISTSIZE
+		fc -R $HISTFILE;
+		date=\$(history -i {1} {1} | awk \"{print \\\$2\\\" \\\"\\\$3}\");
+		printf \"# %s\n# \$date\n\n%s\n\" {1} {2..} | bat -l sh \
+			--style grid,numbers --paging always \
+			--pager \"less --+quit-if-one-screen\"
+	)'
 	--bind 'ctrl-x:transform(
 		$DELETE_TRANSFORM
 		printf \"+print(${SEP}x${SEP})\" {q};
