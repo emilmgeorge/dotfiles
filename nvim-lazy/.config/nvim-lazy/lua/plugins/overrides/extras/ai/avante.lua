eef13387dc3171329bd1b430344e0690ed6ad754
Emil M George <emilmgeorge@gmail.com>
2026-03-09 11:15:59 +0530

    (nvim-lazy) Update avante config

--- nvim-lazy/.config/nvim-lazy/lua/plugins/overrides/extras/ai/avante.lua
+++ nvim-lazy/.config/nvim-lazy/lua/plugins/overrides/extras/ai/avante.lua
@@ -6,8 +6,15 @@ return {
       { "<leader>ae", ":AvanteEdit<CR>", desc = "Edit Avante", mode = { "n", "x", "v" } },
     },
     opts = {
+      provider = "copilot",
       behaviour = {
         auto_approve_tool_permissions = false,
+        confirmation_ui_style = "popup",
+      },
+      windows = {
+        ask = {
+          start_insert = false,
+        },
       },
     }
   },
